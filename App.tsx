576748bc4aff6bc9345b24a72406672f9f6449e4
DOGUK <dgkim3811@naver.com>
2020-09-23 21:28:58 +0900

    AddReactNavtiveSetting

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>네이티브 시작합니다 레벨 업 가쥬아!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +14,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
